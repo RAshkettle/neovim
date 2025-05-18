088c70d8a1c02d7e9e1e55662a4ea726f442b58b
Richard Ashkettle <richard.ashkettle@gmail.com>
2025-05-17 21:36:18 -0400

    turned off sign column

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- If you want to hide the sign column
+vim.opt.signcolumn = 'no'
+
+-- To reset signcolumn (if that was the issue)
+-- vim.opt.signcolumn = 'yes'
