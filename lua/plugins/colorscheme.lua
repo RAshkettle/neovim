5a89ea8a83139cfe3cf3179f5aa0c464115acad5
Richard Ashkettle <richard.ashkettle@gmail.com>
2025-05-18 16:58:26 -0400

    minor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,8 @@
 return {
-  -- Configure colorscheme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,    -- load during startup
-    priority = 1000, -- load this before all other plugins
-    opts = {
-      style = "storm", -- The theme comes in "storm", "moon", "night" and "day" variants
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      -- Apply colorscheme
-      vim.cmd.colorscheme("tokyonight-storm")
-    end,
-  },
+ 
+  { "catppuccin/nvim", enabled = true },
   
-  -- Disable LazyVim's default colorscheme
-  { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight-storm" } },
-  { "catppuccin/nvim", enabled = false },
 }
+
+
+
